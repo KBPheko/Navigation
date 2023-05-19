f7704f900ff70a8d797bcc2493488a16fb1ab444
KBPheko <@J8riW2aG*>
2023-05-19 16:18:29 +0200

    first commit

---
+++ Navigation/HomeVC.swift
@@ -0,0 +1,32 @@
+//
+//  HomeVC.swift
+//  Navigation
+//
+//  Created by DA MAC M1 151 on 2023/05/19.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        //performSegue(withIdentifier: "SecondVC", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Navigation/SecondVC.swift
@@ -0,0 +1,31 @@
+//
+//  SecondVC.swift
+//  Navigation
+//
+//  Created by DA MAC M1 151 on 2023/05/19.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ButtonPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
